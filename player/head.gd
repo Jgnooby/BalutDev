53d425eb823198bee718a0761525eabdc4e68ade
Trace019 <glowseperate2.0@gmail.com>
2026-03-08 14:36:48 +0800

    Added Camera Controller Support

--- player/head.gd
+++ player/head.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 var sensitivity = 0.5
+var contSensitivity = 0.1
+var contDeadzone = 0.15
 var camera : Camera3D
 
 var normal_fov = 75.0
@@ -19,6 +21,19 @@ func _process(delta):
 		target_fov = run_fov
 	
 	camera.fov = lerp(camera.fov, target_fov, delta * fov_lerp_speed)
+	
+	# Controller Capture
+	var joystickX = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
+	var joystickY = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+	# PREVENT STICK DRIFT
+	if abs(joystickX) < .15: 
+		joystickX = 0.0
+	if abs(joystickY) < .15:
+		joystickY = 0.0
+	# Right Controller Movement
+	get_parent().rotate_y(-joystickX * contSensitivity * delta * 60)
+	var pitch = rotation.x - (joystickY * contSensitivity * delta * 60)
+	rotation.x = clamp(pitch, deg_to_rad(-90), deg_to_rad(90))
 
 func _input(event: InputEvent):
 
@@ -30,7 +45,6 @@ func _input(event: InputEvent):
 
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		get_parent().rotate_y(deg_to_rad(-event.relative.x * sensitivity))
-
 		var pitch = rotation.x - deg_to_rad(event.relative.y * sensitivity)
 		pitch = clamp(pitch, deg_to_rad(-90), deg_to_rad(90))
 		rotation.x = pitch
